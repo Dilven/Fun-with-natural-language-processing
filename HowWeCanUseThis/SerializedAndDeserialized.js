f06a15e1d510cee03649499d8e3ecaac60039b86
Dilven <mateusiakkrystian@gmail.com>
2018-09-14 16:01:04 +0200

    remove polish characters

---
+++ HowWeCanUseThis/SerializedAndDeserialized.js
@@ -0,0 +1,12 @@
+var natural = require('natural');
+
+var classifier = new natural.BayesClassifier();
+classifier.addDocument(['sell', 'gold'], 'sell');
+classifier.addDocument(['buy', 'silver'], 'buy');
+classifier.train();
+
+// serialize
+var raw = JSON.stringify(classifier);
+// deserialize
+var restoredClassifier = natural.BayesClassifier.restore(JSON.parse(raw));
+console.log(restoredClassifier.classify('i should sell that'));
--- tokenizer and stemming/index.js
+++ tokenizer and stemming/index.js
@@ -1,25 +1,50 @@
 const natural = require('natural');
 const stemmer = natural.PorterStemmer;
+const tokenizer = new natural.WordTokenizer();
 
 const jiraDescription = "Elasticsearch keyword search research";
 const commitDescription = "Added implementation keyword search";
 
-console.log(stemmer.tokenizeAndStem(jiraDescription));
-console.log(stemmer.tokenizeAndStem(commitDescription));
+// console.log(stemmer.tokenizeAndStem(jiraDescription));
+// console.log(stemmer.tokenizeAndStem(commitDescription));
 
 
-const pluralWord = "cups";
+// const pluralWord = "cups";
 
-console.log(stemmer.tokenizeAndStem(pluralWord));
+// console.log(stemmer.tokenizeAndStem(pluralWord));
 
-const commit = "I added error handling";
+// const commit = "I added error handling";
 
-const sentence = "Im the boss";
+// const sentence = "Im the boss";
 
-console.log(stemmer.tokenizeAndStem(commit));
-console.log(stemmer.tokenizeAndStem(sentence));
+// console.log(stemmer.tokenizeAndStem(commit));
+// console.log(stemmer.tokenizeAndStem(sentence));
 
 
-const stopWords = "Are you there";
+// const stopWords = "Are you there";
 
-console.log(stemmer.tokenizeAndStem(stopWords));
+// console.log(stemmer.tokenizeAndStem(stopWords));
+// console.log(stemmer.tokenizeAndStem("Opportunities"));
+// console.log(stemmer.tokenizeAndStem("Dzieci"));
+// console.log(stemmer.tokenizeAndStem("Dziecko"));
+// console.log(stemmer.tokenizeAndStem("Niektórych"));
+console.log(tokenizer.tokenize('Niektórych łózek nie da się złożyć'))
+console.log(stemmer.tokenizeAndStem('Niektorych lozek nie da się zlozyc'))
+
+// const polishCharacters = [ą, ć, ę, ł, ń, ó, ś, ż, ź ];
+
+var text = "Niektórych łózek nie da się złożyć";
+
+// Replace "the" with "a".
+var result = text
+  .replace(/ą/g, "a")
+  .replace(/ć/g, "c")
+  .replace(/ę/g, "e")
+  .replace(/ł/g, "l")
+  .replace(/ń/g, "n")
+  .replace(/ó/g, "o")
+  .replace(/ś/g, "s")
+  .replace(/ż/g, "z")
+  .replace(/ż/g, "z")
+
+console.log(result);
